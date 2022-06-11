7d4693db3bbc4ce743398bc8e662ed4455291bdc
martina benitez <benitezmartina05@gmail.com>
2022-06-11 19:12:51 -0300

    cards

--- base/menu/views.py
+++ base/menu/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+from menu.models import plato
+
 # Create your views here.
 def menu(request):
-    return render(request, 'menu.html')
+    context = {
+        "platos": plato.objects.all()
+    }
+    return render(request, 'menu.html', context=context)
