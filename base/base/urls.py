b709d5231c4027a0ec79d5d132f6f482faa3d814
martina benitez <benitezmartina05@gmail.com>
2022-06-12 12:39:42 -0300

    herencia de templates

--- base/base/urls.py
+++ base/base/urls.py
@@ -20,7 +20,7 @@ from base.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", index, name="index" ),
+    path('', index, name="index" ),
     path ('menú/', include('menu.urls'))
     
 ]
